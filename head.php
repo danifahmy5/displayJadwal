4c26f132d4e5cc44b68fed2ff7877ade3e9e546c
dRos <danifahmy5@gmail.com>
2021-01-26 09:14:00 +0700

    menghilangkan intregity

--- head.php
+++ head.php
@@ -7,6 +7,6 @@
   <link rel="icon" href="assets/icon/logo.png" type="image/png">
   <link rel="stylesheet" href="assets/style/bootstrap.css">
   <link rel="stylesheet" href="assets/style/style.css">
-  <script src="script/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
-  <script src="script/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  <script src="script/jquery-3.5.1.js"></script>
+  <script src="script/bootstrap.bundle.min.js"></script>
 </head>
